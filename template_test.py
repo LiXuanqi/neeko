0ed5da2761a8442a90dba49f2ac5188b6a889efe
lixuanqi <lixuanqi1995@gmail.com>
2019-02-21 00:15:20 -0800

    add some test cases for exceptions.

--- template_test.py
+++ template_test.py
@@ -1,5 +1,8 @@
+import re
+
 import pytest
 
+from errors import TemplateSyntaxError
 from template import Template
 
 
@@ -8,16 +11,26 @@ def tryRender(text, ctx=None, expected=None):
     if expected:
         assert actual == expected
 
+
+def assertSyntaxError(text, ctx=None, msg=None):
+    with pytest.raises(TemplateSyntaxError) as excinfo:
+        tryRender(text, ctx)
+    assert str(excinfo.value) == msg
+
+
 def test_passthrough():
     tryRender("Hello.", {}, "Hello.")
     tryRender("Hello, 20% fun time!", {}, "Hello, 20% fun time!")
 
+
 def test_variables():
-    tryRender("Hello, {{name}}!", {'name':'1_x7'}, "Hello, 1_x7!")
+    tryRender("Hello, {{name}}!", {'name': '1_x7'}, "Hello, 1_x7!")
+
 
 def test_undefined_variables():
     with pytest.raises(KeyError):
         tryRender("Hi, {{name}}!")
+
 
 def test_pipes():
     data = {
@@ -28,20 +41,23 @@ def test_pipes():
     tryRender("Hello, {{name|upper}}!", data, "Hello, NED!")
     tryRender("Hello, {{name|upper|second}}!", data, "Hello, E!")
 
+
 def test_reusability():
     globs = {
         'upper': lambda x: x.upper(),
         'punct': '!',
     }
     template = Template("This is {{name|upper}}{{punct}}", globs)
-    assert template.render({'name':'Ned'}) == "This is NED!"
-    assert template.render({'name':'Ben'}) == "This is BEN!"
+    assert template.render({'name': 'Ned'}) == "This is NED!"
+    assert template.render({'name': 'Ben'}) == "This is BEN!"
+
 
 def test_attributes():
     obj = AnyOldObject(a="Ay")
     tryRender("{{obj.a}}", locals(), "Ay")
     obj2 = AnyOldObject(obj=obj, b="Bee")
     tryRender("{{obj2.obj.a}} {{obj2.b}}", locals(), "Ay Bee")
+
 
 def test_member_function():
     class WithMemberFns(AnyOldObject):
@@ -51,9 +67,11 @@ def test_member_function():
     obj = WithMemberFns(txt="Once")
     tryRender("{{obj.ditto}}", locals(), "OnceOnce")
 
+
 def test_dict():
     d = {'a': 17, 'b': 23}
     tryRender("{{d.a}} < {{d.b}}", locals(), "17 < 23")
+
 
 def test_loops():
     nums = [1, 2, 3, 4]
@@ -63,17 +81,21 @@ def test_loops():
         "Look: 1, 2, 3, 4, done."
     )
 
+
 def test_loops_with_pipes():
     nums = [1, 2, 3, 4]
+
     def rev(l):
         l = l[:]
         l.reverse()
         return l
+
     tryRender(
         "Look: {% for n in nums|rev %}{{n}}, {% endfor %}done.",
         locals(),
         "Look: 4, 3, 2, 1, done."
     )
+
 
 def test_empty_loops():
     tryRender(
@@ -82,10 +104,14 @@ def test_empty_loops():
         "Empty: done."
     )
 
+
 def test_multiline_loops():
-    "Look: \n{% for n in nums %}\n{{n}}, \n{% endfor %}done.",
-    {'nums': [1, 2, 3]},
-    "Look: \n\n1, \n\n2, \n\n3, \ndone."
+    tryRender(
+        "Look: \n{% for n in nums %}\n{{n}}, \n{% endfor %}done.",
+        {'nums': [1, 2, 3]},
+        "Look: \n\n1, \n\n2, \n\n3, \ndone."
+    )
+
 
 def test_multiple_loops():
     tryRender(
@@ -94,6 +120,7 @@ def test_multiple_loops():
         {'nums': [1, 2, 3]},
         "123 and 123"
     )
+
 
 def test_comments():
     tryRender(
@@ -104,6 +131,7 @@ def test_comments():
         "Hello, {# Name\ngoes\nhere: #}{{name}}!",
         {'name': 'Ned'}, "Hello, Ned!"
     )
+
 
 def test_if():
     tryRender(
@@ -132,6 +160,7 @@ def test_if():
         "Hi, NEDBEN!"
     )
 
+
 def test_complex_if():
     class Complex(AnyOldObject):
         """A class to try out complex data access."""
@@ -151,10 +180,11 @@ def test_complex_if():
         "@XS!"
     )
 
+
 def test_loop_if():
     tryRender(
         "@{% for n in nums %}{% if n %}Z{% endif %}{{n}}{% endfor %}!",
-        {'nums': [0,1,2]},
+        {'nums': [0, 1, 2]},
         "@0Z1Z2!"
     )
     tryRender(
@@ -168,6 +198,7 @@ def test_loop_if():
         "X!"
     )
 
+
 def test_nested_loops():
     tryRender(
         "@"
@@ -179,11 +210,42 @@ def test_nested_loops():
         "@a0b0c0a1b1c1a2b2c2!"
     )
 
+
 def test_exception_during_evaluation():
     with pytest.raises(TypeError):
         tryRender("Hey {{foo.bar.baz}} there", {'foo': None}, "Hey ??? there")
 
+
 def test_bad_names():
+    assertSyntaxError("Wat: {{ var%&!@ }}", {}, "The param's name is invalid.: 'var%&!@'")
+    assertSyntaxError("Wat: {{ foo|filter%&!@ }}", {}, "The param's name is invalid.: 'filter%&!@'")
+    assertSyntaxError("Wat: {% for @ in x %}{% endfor %}", {}, "The param's name is invalid.: '@'")
+
+
+def test_bogus_tag_syntax():
+    assertSyntaxError("Huh: {% bogus %}!!{% endbogus %}??", {}, "Don't understand tag: 'bogus'")
+
+
+def test_malformed_if():
+    assertSyntaxError("Buh? {% if %}hi!{% endif %}", {}, "Invalid If Statement: '{% if %}'")
+    assertSyntaxError("Buh? {% if this or that %}hi!{% endif %}", {}, "Invalid If Statement: '{% if this or that %}'")
+
+
+def test_malformed_for():
+    assertSyntaxError("Weird: {% for %}loop{% endfor %}", {}, "Invalid For Statement: '{% for %}'")
+    assertSyntaxError("Weird: {% for x from y %}loop{% endfor %}", {}, "Invalid For Statement: '{% for x from y %}'")
+    assertSyntaxError("Weird: {% for x, y in z %}loop{% endfor %}", {}, "Invalid For Statement: '{% for x, y in z %}'")
+
+
+def test_bad_nesting():
+    assertSyntaxError("{% if x %}X", {}, "Unmatched action tag: 'if'")
+    assertSyntaxError("{% if x %}X{% endfor %}", {}, "Mismatched end tag: 'for'")
+    assertSyntaxError("{% if x %}{% endif %}{% endif %}", {}, "Too many ends: '{% endif %}'")
+
+
+def test_malformed_end():
+    assertSyntaxError("{% if x %}X{% end if %}", {}, "Invalid End Statement: '{% end if %}'")
+    assertSyntaxError("{% if x %}X{% endif now %}", {}, "Invalid End Statement: '{% endif now %}'")
 
 
 class AnyOldObject:
